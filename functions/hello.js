ff3d9892f947dc49ea3bc780525de6bf4380e650
えるちき <tmuchicken@yahoo.co.jp>
2026-01-18 19:35:34 +0900

    chore: カナリアテスト関数と関連するTypeScript設定ファイルを削除。

--- functions/hello.js
+++
@@ -1,5 +0,0 @@
-// Simple canary function to test if Functions are deploying at all
-// Access at /hello
-export const onRequest = () => {
-  return new Response("Hello from Cloudflare Functions! (Canary Test)");
-};
